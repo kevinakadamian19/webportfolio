f26f590974ecfbc7e8733a88a4f73fc0606beb8d
Kevin Damian Ta <kevindamianta@gmail.com>
2019-06-04 17:52:15 -0700

    Added Content for Projects, and Bio
    Added Content for HTML pages using DOM manipulation. Added social media links.

--- script.js
+++ script.js
@@ -1,26 +1,21 @@
 function createHome() {
 	let home = `
-
 	<header><a href="" id="home-button">Kevin Damian Ta</a></header>
-
   	<div role="main" class="main">
   		<section id="headline">
   			<h1>Welcome!</h1>
   			<p>An aspiring Full Stack Engineer by day, and Leage of Legends enthusiast by night.</p>
   			<p>Take a look some of my work here!</p>
   		</section>
-
   		<section id="projects">
   			<h1>Projects</h1>
   			<input type="image" src="projects.jpg" name="project-button" id="project-button">
   		</section>
-
   		<section id="bio">
   			<h1>About Me</h1>
   			<input type="image" src="bio.jpg" name="bio-button" id="bio-button">
   		</section>
   	</div>
-
   	<script src="https://code.jquery.com/jquery-3.4.1.js" integrity="sha256-WpOohJOqMqqyKL9FccASB9O0KwACQJpFTUBLTYOVvVU=" crossorigin="anonymous"></script>
     <script src="script.js"></script>
   </body>`;
@@ -36,20 +31,17 @@ function clickHome() {
 
 function  createProject() {
 	let create = `
-
   	<div role="main" class="main">
-  		<section id="headline">
-  			<h1>Welcome!</h1>
-  			<p>An aspiring Full Stack Engineer by day, and Leage of Legends enthusiast by night.</p>
-  			<p>Take a look some of my work here!</p>
+  		<section id="project-one">
+  			<h1>Pokemon Quiz App</h1>
+  			<a href="https://kevinakadamian19.github.io/pokemon-master-quiz-app/"><img src="quizproject.jpg"></a>
+  			<p>
+  				I made a 6 question quiz that asks questions Pokemon trivia questions.
+  				I used HTML, CSS, and Javascript implmenting jQuery for DOM manipulation.
+  			</p>
   		</section>
-
-  		<section id="projects">
-  			<input type="image" src="projects.jpg" name="project-button" id="project-button">
-  		</section>
-
-  		<section id="bio">
-  			<input type="image" src="bio.jpg" name="bio-button" id="bio-button">
+  		<section id="tbd">
+  			<h1>New Projects Coming Soon!</h1>
   		</section>
   	</div>`;
 
@@ -66,11 +58,10 @@ function clickProject() {
 
 function createBio() {
 	let bio = `
-
 <div role="main" class="main">
   		<section id="headshot-photo">
+  		<img src="fullheadline.jpg" alt="Headshot photo of me; Kevin Damian Ta">
   		</section>
-
   		<section id="bio-description">
   			<p> 
   				I am a web developer that is currently attending the Full Stack Flex program offered by Thinkful. 
@@ -78,9 +69,11 @@ function createBio() {
   				I love seafood, chicken wings, and all things Anime related.
   			</p>
   		</section>
-
-  		<section id="bio">
-  			
+  		<section id="social-media">
+  			<ul class="social-media-links">
+				<li><a href="https://github.com/kevinakadamian19"><img src="github.jpg"></a></li>
+				<li><a href="https://www.linkedin.com/in/kevindamianta/"><img src="linkedin.jpg"></a></li>
+			</ul>
   		</section>
   	</div>`;
 
@@ -103,3 +96,26 @@ function renderPage() {
 }
 
 renderPage();
+
+
+
+function viewHome() {
+	$('.main').on('click', '#home-button', function(event) {
+		$('.main').remove();
+	});
+
+}
+
+function viewProjects() {
+
+}
+
+function viewAboutMe() {
+
+}
+
+function renderPortfolio() {
+
+}
+
+renderPortfolio();
