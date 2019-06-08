e35a05a9d15e105b808d249c5cab9d0871a79bcd
Kevin Damian Ta <kevindamianta@gmail.com>
2019-06-07 17:28:06 -0700

    Implemented Feedback provided by Thinkful Review
    Made responsive design to where the border box respond to width of the screen.
    
    Adjusted so that even the smallest screen doesnt have text overlap
    Updated the About Me section to include my current work, and where I want to go.
    Added Email link in contact list.
    Had all hyperlinks open in new tab.
    Project section has link for Github repository.

--- script.js
+++ script.js
@@ -1,22 +1,22 @@
 function createHome() {
-	let home = `
-	<header><a href="" id="home-button">Kevin Damian Ta</a></header>
-  	<div role="main" class="main">
-  		<section id="headline">
-  			<h1>Welcome!</h1>
-  			<p>An aspiring Full Stack Engineer by day, and Leage of Legends enthusiast by night.</p>
-  			<p>Take a look some of my work here!</p>
-  		</section>
-  		<section id="projects">
-  			<h1>Projects</h1>
-  			<input type="image" src="projects.jpg" name="project-button" id="project-button">
-  		</section>
-  		<section id="bio">
-  			<h1>About Me</h1>
-  			<input type="image" src="bio.jpg" name="bio-button" id="bio-button">
-  		</section>
-  	</div>
-  	<script src="https://code.jquery.com/jquery-3.4.1.js" integrity="sha256-WpOohJOqMqqyKL9FccASB9O0KwACQJpFTUBLTYOVvVU=" crossorigin="anonymous"></script>
+  let home = `
+  <header><a href="" id="home-button">Kevin Damian Ta</a></header>
+    <div role="main" class="main">
+      <section id="headline">
+        <h1>Welcome!</h1>
+        <p>An aspiring Full Stack Engineer by day, and Leage of Legends enthusiast by night.</p>
+        <p>Take a look some of my work here!</p>
+      </section>
+      <section id="projects">
+        <h1>Projects</h1>
+        <input type="image" src="projects.jpg" name="project-button" id="project-button">
+      </section>
+      <section id="bio">
+        <h1>About Me</h1>
+        <input type="image" src="bio.jpg" name="bio-button" id="bio-button">
+      </section>
+    </div>
+    <script src="https://code.jquery.com/jquery-3.4.1.js" integrity="sha256-WpOohJOqMqqyKL9FccASB9O0KwACQJpFTUBLTYOVvVU=" crossorigin="anonymous"></script>
     <script src="script.js"></script>
   </body>`;
 
@@ -24,26 +24,27 @@ function createHome() {
 }
 
 function clickHome() {
-	$('body').on('click', '#home-button', function(event) {
-		
-	});
+  $('body').on('click', '#home-button', function(event) {
+    
+  });
 }
 
 function  createProject() {
-	let create = `
-  	<div role="main" class="main">
-  		<section id="project-one">
-  			<h1>Pokemon Quiz App</h1>
-  			<a href="https://kevinakadamian19.github.io/pokemon-master-quiz-app/"><img src="quizproject.jpg"></a>
-  			<p>
-  				I made a 6 question quiz that asks questions Pokemon trivia questions.
-  				I used HTML, CSS, and Javascript implmenting jQuery for DOM manipulation.
-  			</p>
-  		</section>
-  		<section id="tbd">
-  			<h1>New Projects Coming Soon!</h1>
-  		</section>
-  	</div>`;
+  let create = `
+    <div role="main" class="main">
+      <section id="project-one">
+        <h1>Pokemon Quiz App</h1>
+        <a href="https://kevinakadamian19.github.io/pokemon-master-quiz-app/"><img src="quizproject.jpg"></a>
+        <p>
+          I made a 6 question quiz that asks questions Pokemon trivia questions.
+          I used HTML, CSS, and Javascript implmenting jQuery for DOM manipulation.
+        </p>
+        <a href="https://github.com/kevinakadamian19/pokemon-master-quiz-app" id="project-one-repo" target="_blank"><img src="github.jpg" alt="Link to github repository for the Pokemon Master Quiz project."></a>
+      </section>
+      <section id="tbd">
+        <h1>New Projects Coming Soon!</h1>
+      </section>
+    </div>`;
 
   return create;
 };
@@ -51,48 +52,57 @@ function  createProject() {
 function clickProject() {
 
  $('.main').on('click', '#project-button', function(event) {
- 	$('.main').remove();
- 	$('body').append(createProject());
+  $('.main').remove();
+  $('body').append(createProject());
  });
 }
 
 function createBio() {
-	let bio = `
+  let bio = `
 <div role="main" class="main">
-  		<section id="bio-pic">
-  		<img src="power.jpg" alt="Headshot photo of me; Kevin Damian Ta">
-  		</section>
-  		<section id="bio-description">
-  			<p> 
-  				I am a web developer that is currently attending the Full Stack Flex program offered by Thinkful. 
-  				On my downtime I enjoy playing Magic The Gathering, watching the Golden State Warriors, and compete in powerlifting. 
-  				I love seafood, chicken wings, and all things Anime related.
-  			</p>
-  		</section>
-  		<section id="social-media">
-  			<ul class="social-media-links">
-				<li><a href="https://github.com/kevinakadamian19"><img src="github.jpg"></a></li>
-				<li><a href="https://www.linkedin.com/in/kevindamianta/"><img src="linkedin.jpg"></a></li>
-			</ul>
-  		</section>
-  	</div>`;
+      <section id="bio-pic">
+      <img src="power.jpg" alt="Headshot photo of me; Kevin Damian Ta">
+      </section>
+      <section id="bio-description">
+        <p>
+          I am a web developer that is currently attending the Full Stack Flex program offered by Thinkful.
+          I am also working full time for FutureAdvisor which is a FinTech startup that is a subsidiary under BlackRock.
+        </p>
+        <p>
+          I have experienced the growth pains of a successful company, and hope to bring the knowledge I have to my next team.
+          My current goal is to gain enough experience to work for Riot Games as I have been an avid League of Legends player for the past 7 years.
+        </p>
+        <p>
+          On my downtime I enjoy playing Magic The Gathering, watching the Golden State Warriors, and compete in powerlifting. 
+          I am a Year Up alumni, and enjoy spending time closing the opportunity divide by promoting the program's mission to inform the masses.
+          If granted the opportunity, I love to present as I was a member of BlackRock's Toastmaster's club and am also a regular attendee of The Moth Live Sessions.
+        </p>
+      </section>
+      <section id="social-media">
+        <a href="mailto:kevindamianta@gmail.com">Want to get in touch? Shoot me an email!</a>
+        <ul class="social-media-links">
+        <li><a href="https://github.com/kevinakadamian19" target="_blank"><img src="github.jpg" alt="Github logo that will take you to my Github profile when selected."></a></li>
+        <li><a href="https://www.linkedin.com/in/kevindamianta/" target="_blank"><img src="linkedin.jpg" alt="LinkedIn logo that will take you to my LinkedIn profile when selected"></a></li>
+      </ul>
+      </section>
+    </div>`;
 
-  	return bio;
+    return bio;
 }
 
 function clickBio() {
 
 $('.main').on('click', '#bio-button', function(event) {
- 	$('.main').remove();
- 	$('body').append(createBio());
+  $('.main').remove();
+  $('body').append(createBio());
 
  });
 }
 
 function renderPage() {
-	clickBio();
-	clickProject();
-	clickHome();
+  clickBio();
+  clickProject();
+  clickHome();
 }
 
 renderPage();
@@ -100,9 +110,9 @@ renderPage();
 
 
 function viewHome() {
-	$('.main').on('click', '#home-button', function(event) {
-		$('.main').remove();
-	});
+  $('.main').on('click', '#home-button', function(event) {
+    $('.main').remove();
+  });
 
 }
 
